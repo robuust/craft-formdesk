21428fc7ed3d9b0f6ad54033fd14b4bd7626bcf3
Bob Olde Hampsink <bob@robuust.digital>
2022-05-20 10:46:56 +0200

    Submit controller

---
+++ src/controllers/SubmitController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace robuust\formdesk\controllers;
+
+use Craft;
+use craft\web\Controller;
+use craft\web\Response;
+
+/**
+ * Submit controller.
+ */
+class SubmitController extends Controller
+{
+    public $allowAnonymous = true;
+
+    /**
+     * Submit form to Formdesk.
+     *
+     * @return Response|null
+     */
+    public function actionIndex(): ?Response
+    {
+        $this->requirePostRequest();
+
+        $values = $this->request->getBodyParams();
+        $list = $values['list_id'];
+
+        $errors = null;
+        try {
+            $result = $this->module->formdesk->post("forms/{$list}/results/?process_messages=true", [
+                'json' => array_slice($values, 1),
+            ]);
+        } catch (\Exception $e) {
+            $errors = $e;
+        }
+
+        if ($errors) {
+            if ($this->request->getAcceptsJson()) {
+                return $this->asJson([
+                    'errors' => $errors,
+                ]);
+            }
+
+            // Send the entry back to the template
+            Craft::$app->getUrlManager()->setRouteParams([
+                'errors' => $errors,
+            ]);
+
+            return null;
+        }
+
+        return $this->redirectToPostedUrl((object) $result);
+    }
+}
